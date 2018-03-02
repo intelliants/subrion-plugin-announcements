2a7ba19ecd6bb2b908c4cb2897c41f37f6e18952
Vladimir <semchishin.vladimir@gmail.com>
2018-03-02 15:13:31 +0600

    Bug Fixed. close  #2

--- admin/index.php
+++ admin/index.php
@@ -55,7 +55,7 @@ class iaBackendController extends iaAbstractControllerModuleBackend
 
         return parent::_entryAdd($entryData);
     }
-
+ 
     public function updateCounters($entryId, array $entryData, $action, $previousData = null)
     {
         $this->_iaCore->factory('cache')->remove('announcements');
